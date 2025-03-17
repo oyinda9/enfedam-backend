3e3cbe1125cb9f6be4a81615966e4a688806d244
sholanke precious <sholankeprecious3@gmail.com>
2025-03-17 15:26:22 +0100

    parent id

--- src/routes/ParentRoutes.ts
+++ src/routes/ParentRoutes.ts
@@ -4,6 +4,7 @@ import {
   getAllParents,
   updateParent,
   deleteParent,
+  getParentById
 } from "../controllers/ParentController";
 import {
   authenticateAdmin,
@@ -16,6 +17,7 @@ const router = express.Router();
 // Apply middleware to routes
 router.post("/", authenticateAdmin, authorizeAdmin, authorizeParentCreation,createParent); // Only admins can create parents
 router.get("/", getAllParents); // Public route (no authentication required)
+router.get("/:id", getParentById); // Public route (no authentication required)
 router.put("/:id", authenticateAdmin, authorizeAdmin, updateParent); // Only admins can update parents
 router.delete("/:id", authenticateAdmin, authorizeAdmin, deleteParent); // Only admins can delete parents
 
