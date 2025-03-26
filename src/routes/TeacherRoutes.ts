3ccc53a9c5f7632718c847cf4f4183ad8de8d16f
sholanke precious <sholankeprecious3@gmail.com>
2025-03-26 06:04:51 +0100

    changed patch to put

--- src/routes/TeacherRoutes.ts
+++ src/routes/TeacherRoutes.ts
@@ -17,7 +17,7 @@ const router = Router();
 router.post("/", authenticateAdmin, authorizeAdmin, authorizeTeacherCreation, createTeacher );
 router.get("/", getTeachers , authenticateAdmin, authorizeAdmin, authorizeTeacherCreation,);
 router.get("/:id", getTeacherById, authenticateAdmin, authorizeAdmin);
-router.put("/:id", authenticateAdmin, authorizeAdmin, authorizeTeacherCreation, updateTeacher);
+router.patch("/:id", authenticateAdmin, authorizeAdmin, authorizeTeacherCreation, updateTeacher);
 router.delete("/:id", authenticateAdmin, authorizeAdmin, authorizeTeacherCreation, deleteTeacher);
 
 export default router;
