c43e16544c6eaf5b8f344e081b1c28c7487cbd29
sholanke precious <sholankeprecious3@gmail.com>
2025-03-31 16:35:31 +0100

    added get all events

--- src/routes/EventRoutes.ts
+++ src/routes/EventRoutes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import {createEvent,} from "../controllers/EventsController"
+import {createEvent,getAllEvents} from "../controllers/EventsController"
 const router = Router();
 router.post("/",createEvent)
+router.get("/",getAllEvents)
 
 
 export default router
