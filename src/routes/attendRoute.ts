f93f90f1bee601286208a1cd80f5785b1766a36e
sholanke precious <sholankeprecious3@gmail.com>
2025-04-09 13:49:41 +0100

    please work

--- src/routes/attendRoute.ts
+++ src/routes/attendRoute.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { createAttendance } from '../controllers/attendanceController'; // Make sure this path is correct
-
+import { createAttendance } from "../controllers/attendanceController"
 const router = express.Router();
 
 router.post('/attendance', createAttendance); // ✅ Uses your real controller function
